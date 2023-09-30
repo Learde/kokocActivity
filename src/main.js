d68c3c3e5c0c6ae35b4992d9c42d1a7c215e6666
Learde <leardeleo@gmail.com>
2023-09-30 14:02:46 +0300

    Change vh on resize

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 import app from "@/app";
 
 app.mount("#app");
+window.addEventListener("resize", () => {
+    const vh = window.innerHeight * 0.01;
 
-const vh = window.innerHeight * 0.01;
-
-document.documentElement.style.setProperty("--vh", `${vh}px`);
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+});
